489269bcf47ec074a52982510de0fc3f0cf34800
reperfection <wodhkszxc@gmail.com>
2022-10-17 21:25:35 +0900

    edit views.py

--- it/views.py
+++ it/views.py
@@ -17,7 +17,6 @@ def itSubmit(request):
         major_id = int(request.POST[f'question-{n}'][0])
         counter[major_id] += 1
         
-    # 최고점 개발 유형
     max_value = max(counter)
     best_major_id = counter.index(max_value)
     best_major = IT.objects.get(pk=best_major_id)
@@ -51,7 +50,6 @@ def itEngSubmit(request):
         major_id = int(request.POST[f'question-{n}'][0])
         counter[major_id] += 1
         
-    # 최고점 개발 유형
     max_value = max(counter)
     best_major_id = counter.index(max_value)
     best_major = IT_eng.objects.get(pk=best_major_id)
@@ -84,9 +82,8 @@ def itChSubmit(request):
         major_id = int(request.POST[f'question-{n}'][0])
         counter[major_id] += 1
         
-    # 최고점 개발 유형
-    
-    best_major_id = max(range(1, K+1), key=lambda id : counter[id])
+    max_value = max(counter)
+    best_major_id = counter.index(max_value)
     best_major = IT_ch.objects.get(pk=best_major_id)
     best_major.count += 1
     best_major.save()
